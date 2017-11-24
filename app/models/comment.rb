ecb194e5e2ae36017bf17493a0bbe814726ebcd0
John Tuttle <thejwt@gmail.com>
2017-11-24 23:50:53 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,9 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  def user=(user1)
+    user = User.find_or_create_by(user1)
+    self.categories << category
+  end
+
 end
