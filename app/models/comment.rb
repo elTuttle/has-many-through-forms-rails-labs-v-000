39e1d6947429345fb7fb14f0743330bf13ff6c44
John Tuttle <thejwt@gmail.com>
2017-11-25 00:59:35 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,11 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
   accepts_nested_attributes_for :user
-  before_save :erase_empty_ingredients
-
-
-  def erase_empty_user
-    self.user = self.user.select {|i| i.username && i.username != '' || i.content && i.content != ''}
-  end
 
 end
