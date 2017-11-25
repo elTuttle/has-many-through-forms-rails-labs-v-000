ee30fe803e3e7797e941cb1fb2ef82bad5bc2439
John Tuttle <thejwt@gmail.com>
2017-11-25 01:06:42 +0000

    automatically backed up by learn

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, user_attributes:[:username])
   end
 
   def comment_params_with_id
